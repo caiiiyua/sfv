5cc760d02eb7e9bc03c1085df3c7045e0fa0a6ee
Yuanqing Cai <ckjacket@gmail.com>
2015-06-26 01:33:14 +0800

    first version of sfv

--- public/js/myapp.js
+++ public/js/myapp.js
@@ -2,6 +2,22 @@
 	var app = angular.module('myapp', []);
 	app.controller('MainController', function() {
 		this.message = "hello";
-		this.user_num = 1;
+		this.users = users;
+		this.user_count = users.length;
+		this.email = "";
+
+		this.addUser = function() {
+			if (this.email.length > 0) {				
+			users.push(this.email);
+			this.email = "";
+			this.users = users;
+			this.user_count = users.length;
+			};
+		};
 	});
+
+	var users = [
+		'ckjacket@mm.com',
+		'kkk@ls.com'
+	];
 })();
